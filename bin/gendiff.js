57d854b1ce1c1c7589cc7b68a2da9ce9209a99b1
SnowSnail <f2961@yandex.ru>
2022-12-24 19:16:45 +0300

    Add basic functionality (Flat JSONs diffs)

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import genDiff from '../src/index.js';
 
 const program = new Command();
 
@@ -7,6 +8,10 @@ program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format');
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => {
+    const diff = genDiff(filepath1, filepath2);
+    console.log(diff);
+  });
 
 program.parse();
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import _ from 'lodash';
+import { readFileSync } from 'fs';
+import { cwd } from 'process';
+import path from 'path';
+
+const genDiff = (filepath1, filepath2) => {
+  const parseJson = (filepath) => JSON.parse(readFileSync(path.resolve(cwd(), filepath), 'utf-8'));
+
+  const json1 = parseJson(filepath1);
+  const json2 = parseJson(filepath2);
+  const allKeysSorted = _.sortBy(_.union(Object.keys(json1), Object.keys(json2)));
+  const diffsArray = allKeysSorted.map((key) => {
+    const isKeyInJson1 = _.has(json1, key);
+    const isKeyInJson2 = _.has(json2, key);
+    if (isKeyInJson1 && !isKeyInJson2) {
+      return `  - ${key}: ${json1[key]}`;
+    }
+    if (!isKeyInJson1 && isKeyInJson2) {
+      return `  + ${key}: ${json2[key]}`;
+    }
+    if (isKeyInJson1 && isKeyInJson2 && json1[key] === json2[key]) {
+      return `    ${key}: ${json1[key]}`;
+    }
+    return `  - ${key}: ${json1[key]}\n  + ${key}: ${json2[key]}`;
+  });
+  return `{\n${diffsArray.join('\n')}\n}`;
+};
+
+export default genDiff;
